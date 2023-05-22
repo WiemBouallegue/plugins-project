99313a9f389a95be7e321a5e74782e66dfe2c8bf
Wiem-Bouallegue <wiem.bouallegue@kyndryl.com>
2023-05-23 00:49:02 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { DataContext, DataContextProvider } from './Context';
 import Main from './Main';
+import { QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
-  return <Main />;
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>
+      <DataContextProvider>
+        <Main />
+      </DataContextProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,4 +1,49 @@
+import SideNav from './components/sidenav/SideNav';
+import BasicPage from './pages/BasicPage';
+import { useContext } from 'react';
+import { DataContext } from './Context';
+import { getTabDetails } from './utility/data';
+import { DataRes } from './types';
+import { Route, Routes } from 'react-router-dom';
+
 const Main = (): JSX.Element => {
-  return <div></div>;
+  const data: DataRes = useContext(DataContext);
+  const tabsDetails = getTabDetails(data!);
+  if (!tabsDetails) {
+    return <h1>Loading</h1>;
+  }
+  return (
+    <div>
+      <SideNav tabDetails={tabsDetails!} />
+      <Routes>
+        {tabsDetails && tabsDetails[0] && (
+          <Route
+            path={'/'}
+            element={
+              <BasicPage
+                title={tabsDetails[0].title}
+                plugins={tabsDetails[0].AllPlugins}
+                tabId={tabsDetails[0].tabId}
+              />
+            }
+          />
+        )}
+
+        {tabsDetails!.map((tab) => (
+          <Route
+            key={tab.title}
+            path={tab.title}
+            element={
+              <BasicPage
+                title={tab.title}
+                plugins={tab.AllPlugins}
+                tabId={tab.tabId}
+              />
+            }
+          />
+        ))}
+      </Routes>
+    </div>
+  );
 };
 export default Main;
