5f94043a74d623100636e0971392c15b93d361e2
Wiem-Bouallegue <wiem.bouallegue@kyndryl.com>
2023-05-23 00:09:30 +0200

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    colors: {
+      error: '#da1e28',
+      spring_green: '#4cdd84',
+      oxford_blue: '#00224B',
+      dark_stone: '#3d3c3c',
+      light_gray: '#E9EAEC',
+      white: '#fff',
+      black: '#000',
+      steel_blue: '#2E75B5',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
